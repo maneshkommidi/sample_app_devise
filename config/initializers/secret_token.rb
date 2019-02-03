767f335d7a462dde19edda0cffa2ca40280ba1b8
MKommidi <maneshkommidi@gmail.com>
2019-02-02 19:43:18 -0500

    sign-in & sign-up mechanism using devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '75ef40535a17fdcff26175a82098305990792df226dffacc0ceb0c89324142749fb7b68e242d2b0b21e1c1dcbac501d444fafa0e9a6af9a0dee4184206f1811b'
